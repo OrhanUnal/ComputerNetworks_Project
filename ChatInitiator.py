6102313ebead2fdccbd3533ff9f035796c031f53
OrhanUnal <140164675+OrhanUnal@users.noreply.github.com>
2025-04-26 20:57:19 +0300

    Secure chat bitti kullanicinin adini son giris tarihini ip adresini alip mesaj yazildiktan sonra da connectionu kapatiyor. connection acik kalabilir ama hoca kapatin dedigi icin kapanacak sekilde yaptim ancak dikkat edin KEY SUAN CALISMIYOR BUNA BAKILMASI LAZIM. key hesaplaniyor ancak hesaplandiktan sonra encode yaparken kullanmiyoruz sacma bir bicimde bunun nasil calismasi gerektigini bulup duzeltmemiz gerekiyor ONEMLI!!!!

--- ChatInitiator.py
+++ ChatInitiator.py
@@ -5,11 +5,12 @@ from socket import *
 
 discovered_users = {}
 
-serverIP = '127.0.0.1'
+serverIP = '192.168.127.12'
 serverPort = 6001
+
+clientsocket = socket(AF_INET, SOCK_STREAM)
+
 def secure_chat():
-  clientsocket = socket(AF_INET, SOCK_STREAM)
-  clientsocket.connect((serverIP, serverPort))
   publickey = input('Input key: ')
   dictionary = {
     "key": publickey
@@ -18,14 +19,15 @@ def secure_chat():
   clientsocket.send(json_object.encode())
   data = clientsocket.recv(1024).decode()
   received_key = json.loads(data)
-  key=((2^int(publickey)%19)^int(received_key['key']))%19
+  key=((2^int(publickey)%19)^int(received_key['KEY']))%19
   print("Enter your message: ")
   message = input()
   encrypted_message = {
-    "encrypted_message": base64.b64encode(message.encode()).decode()
+    "encrypted_message": base64.b64encode(message.encode()).decode(),
   }
   json_message = json.dumps(encrypted_message,indent=1)
   clientsocket.send(json_message.encode())
+
 def display_users():
   current_time = time.time()
   with open('discovered.json','r') as outfile:
@@ -37,21 +39,26 @@ def display_users():
         elif info == 'timestamp' and current_time - data[username][info] < 900:
           print(f"{username} " + "Away")
 
-secure_chat()
-
-mode = input("Please enter Users, History or Chat")
-if mode == "Users":
-  display_users()
-elif mode == "History":
-  pass
-elif mode == "Chat":
-  chatuser=input("Please enter Username")
-
-  with open('discovered.json','r') as outfile:
-    data = json.load(outfile)
-    for username in data:
-      for info in data[username]:
-        if info == 'username'
-          print(f"{username} " + "Online")
-        elif info == 'username'
-          print(f"{username} " + "Away")
+while True:
+  mode = input("Please enter Users, History or Chat: ").lower()
+  if mode == "users" or mode == "user":
+    display_users()
+  elif mode == "history":
+    pass
+  elif mode == "chat":
+    chat_user = input("Please enter Username: ")
+    with open('discovered.json','r') as outfile:
+      data = json.load(outfile)
+      for username in data:
+        if username == chat_user and time.time() - data[username]['timestamp'] < 900:
+          serverIP = data[username]['ip']
+          clientsocket.connect((serverIP, serverPort))
+          security = input("Do you want to chat securely or not?\n" + "Please enter Secure or Unsecure: ").lower()
+          if security == "secure":
+            secure_chat()
+          else:
+            #unsecure_chat()
+            pass
+          clientsocket.close()
+        else:
+          print("That username is not registered or the person you are trying to reach is not available")
